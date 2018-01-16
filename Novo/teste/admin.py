49e2f2d647c908fe369405aafbd497942a16e5da
ravy.cunha <ravy.cunha@BHD050102384-.bh01.local>
2018-01-16 11:31:05 -0200

    inicio

---
+++ Novo/teste/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from teste.models import *
+# Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Historico)
---
+++ Novo/teste/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Usuario(models.Model): 
+    nome = models.CharField(max_length=30)
+    cpf = models.CharField(max_length = 11)
+    email = models.CharField(max_length = 30)
+    
+    def __str__(self):
+        return self.nome
+    
+class Historico(models.Model):
+    nome = models.CharField(max_length = 20)
+    tipo = models.CharField(max_length = 12)
+    usuario = models.ForeignKey(Usuario,on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.nome
